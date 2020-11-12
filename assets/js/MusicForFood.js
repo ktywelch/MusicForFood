f7f057869814b32bd0dacc6f8ecc6fb1ec388d37
Kathleen Welch <ktywelch@gmail.com>
2020-11-12 14:10:10 -0800

    Renamed the files and updated html to refelct the new names

--- assets/js/MusicForFood.js
+++ assets/js/MusicForFood.js
@@ -1,4 +1,6 @@
-var testVar=""
+/* script sets up arrays for images to depict cultures we want to represent on the app and to control the playlists 
+because the APIs we looked at for music do not specify culture but rahter music Genre and when searching it is difficult to decern a 
+culture (like French from an artist or group with French in the name) so we manually selected the PL from the provider */
 const cultures = {
 	French: {
 		images: ["anthony-delanoix-Q0-fOL2nqZc-unsplash.jpg","adrien-tutin-hCh_PHIhoLI-unsplash.jpg",
@@ -7,7 +9,7 @@ const cultures = {
 		altImage: ["Eiffel Tower Picture taken by Anthony Delanoix","Castle Wall by Adrien Tutin unsplash",
 		"Photo of the Louvre by Daniele D Andreti unsplash","Lavendar field by Leonard Cotte unsplash",
 		"Macrons by Niclas Illg unsplash","La Rochelle by Romain Huneau unsplash"],
-		playlist: [3748969686,6553706304]//,1737848082,7881922322,5574391562]
+		playlist: [3748969686,6553706304,1737848082,7881922322,5574391562]
 	},
 	Indian: {
 		images: ["annie-spratt-w3CyGs-keEM-unsplash.jpg","debashis-rc-biswas-xAWb6qDCXDU-unsplash.jpg","julian-yu-_WuPjE-MPHo-unsplash.jpg",
@@ -15,7 +17,7 @@ const cultures = {
 		altImage: ["Indian Building taken by Annie Spratt","Festival of Color by Debashis Biswas unsplash",
 		"Taj Mahal by Julian Yuyu-_WuPjE-MPHo-unsplash","Walled City of JaiPur by  Kirti Kalla unsplash",
 		"Victoria Memorial by Martin Jernberg unsplash","Statue of Ganesh Sonika Agarwal unsplash.jpg",],
-		playlist: [808923603,7723841822]//,5397210322,2484275588,6124514764]
+		playlist: [808923603,7723841822,5397210322,2484275588,6124514764]
 	},
 	Mexican:  {
 		images: ["jimmy-baum-NjdpeYDHNrQ-unsplash.jpg","2-mexican-fiesta-sushobha-jenner-canvas-print.jpg",
@@ -24,7 +26,7 @@ const cultures = {
 		altImage: ["Mayan Temple taken by Jimmy Baum","Mexican Fiesta on Canvas Print","Painting man in sombrero sleeping in desert",
 		"taco burrito chips and salsa print","cartoon doodles hand drawn mexican food illustration from Doodles Vector Art",
 	    "Mexican Flag","Mexican food images"],
-		playlist: [5212851564,4923634428]//,3712532246,4522361286,7987112122]
+		playlist: [5212851564,4923634428,3712532246,4522361286,7987112122]
 	},
 	Chinese:  {
 		images: ["alexander-schimmeck-gUtcrNunbCM-unsplash.jpg","cat-crawford-FRSSDv3mAy8-unsplash.jpg",
@@ -34,7 +36,7 @@ const cultures = {
 		altImage: ["Terracotta Army taken by Alexander Schimmeck","The Great Wall of China by Cat Crawford",
 				"Chinese Latern by Makus Winkler","Traditioan Fishing by Sam Beasley","Reflected Mountain by Shane Young",
 			    "Chinese Temple by Victor He","Oriental Pearl Tower by Yiranding"],
-		playlist: [4962135028,6224909624]//,969940552,6431991064,6885856824,6324317944]
+		playlist: [4962135028,6224909624,969940552,6431991064,6885856824,6324317944]
 	},
 	Italian:  {
 		images: ["italian images.jpg","BumperSticker.jpg","Italian-12icons.jpg","ItalianFlag.jpg",
@@ -43,7 +45,7 @@ const cultures = {
 		altImage: ["Image of pizza wirh italian flag","Italian Bumper Sticker","Italian popster with 12 food icons",
 		"Italian flag projected on building","Poster of Italian Food","Colleseum by Braden Collum unsplash",
 		"Venice Canals by Damiano Baschiera","italiancuisine traditional poster","Riomaggiore by Jack Ward"],
-		playlist: [4779177244,8146526982]//,5353448802,6514155104,272400133]
+		playlist: [4779177244,8146526982,5353448802,6514155104,272400133]
 	}
 }
 
